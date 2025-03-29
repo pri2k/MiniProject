a5f96115eb675d86abc158a0926ea6d7dd30851e
chakma21 <shinychakma2003@gmail.com>
2025-03-30 02:18:13 +0530

    updated models and added a new script and uploaded images on db

--- src/models/illusion.js
+++ src/models/illusion.js
@@ -1,11 +1,11 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const IllusionSchema = new mongoose.Schema({
-  imageFilename: String, // Link to the illusion image
+  filename: String, // Link to the illusion image
   question: String, // The question related to the image
   options: Object, // Choices {"Face": 5, "Tree": 3}
 });
 
 const Illusion = mongoose.model("Illusion", IllusionSchema);
 
-module.exports = Illusion;
+export default Illusion;
--- src/models/question.js
+++ src/models/question.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const QuestionSchema = new mongoose.Schema({
     category: { type: String, required: true },  // e.g., "Relationships", "Work Stress"
