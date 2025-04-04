34d8528d72c12c2afecbaa86bbf9ab07e7c62ccf
chakma21 <shinychakma2003@gmail.com>
2025-04-04 14:18:15 +0530

    illuison game progressed

--- src/models/illusion.js
+++ src/models/illusion.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 
 const IllusionSchema = new mongoose.Schema({
   filename: String, // Link to the illusion image
+  imageData: { type: String, required: true }, // Base64 image data
+
   question: String, // The question related to the image
   options: Object, // Choices {"Face": 5, "Tree": 3}
 });
