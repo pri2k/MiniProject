b98b1b7c5f61350ad5fb99408e65ad9bf114f1a6
chakma21 <shinychakma2003@gmail.com>
2025-04-05 20:16:40 +0530

    added basic questionnaire interface

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import HowToUse from "@/components/how-use/HowToUse";
 import LandingPage from "@/components/LandingPage";
 import FrequentlyAskedQ from "@/components/faq/FrequentlyAskedQ";
 import VolunteerCall from "@/components/VolunteerCall"
+import Questionnaire from "@/components/Questionnaire";
 
 export default function Home() {
   return (
     <div className="">
       <LandingPage />
       <HowToUse />
+      <Questionnaire/>
       <VolunteerCall/>
       <FrequentlyAskedQ />
     </div>
